198feae16f8541ff9cc08e43106242b4f22d8804
maralynsan <mewskyzza@gmail.com>
2023-08-18 20:02:49 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AuthController extends Controller
+{
+    public function showLogin(){
+        return view('content.login');
+    }
+
+    public function checkLogin(Request $request){
+        $credentials = $request->validate([
+            //required เป็นค่าว่างไหม รูปแบบเป็น email
+            'email' => ['required','email'],
+            'password' => ['required'],
+        ]);
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended('/content');
+        }
+        return back()->withErrors([
+            'email'=>'Creadebtuaks do not match our records',
+        ]);
+    }
+    public function showLogout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return view('content.login');
+    }
+}
